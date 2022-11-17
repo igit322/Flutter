e03be7a2d9905180dbd7d128d8ec93ef39498b90
InGyeong <kimig0322@naver.com>
2022-11-17 22:09:39 +0900

    1117
    퀴즈앱만들기1

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:netflix/screen/screen_home.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter 퀴즈앱',
+        // theme: ThemeData(
+        //   primarySwatch: Colors.blueGrey,
+        // ),
+        home: HomeScreen());
+  }
+}
---
+++ lib/screen/screen_home.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    Size screenSize = MediaQuery.of(context).size;
+    double width = screenSize.width;
+    double height = screenSize.height;
+
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('My Quiz App'),
+          // backgroundColor: Color.deepPurple,
+          leading: Container(), // 자동으로 뒤로가기버튼 생기는거 지움
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Center(
+              child: Image.asset(
+                'images/astronomy-1867616_1920.jpg',
+                width: width * 0.8,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(width * 0.024),
+            ),
+            Text(
+              '플러터 퀴즈앱',
+              style: TextStyle(
+                fontSize: width * 0.065,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            // ignore: prefer_const_constructors
+            Text(
+              '퀴즈를 풀기 전 안내사항',
+              textAlign: TextAlign.center,
+            ),
+            Padding(
+              padding: EdgeInsets.all(width * 0.048),
+            ),
+            _buildStep(width, '1. 랜덤으로 나오는 퀴즈 3개를 풀어보세요'),
+            _buildStep(width, '2. 문제를 읽고 다음 문제 버튼을 클릭해주세요'),
+            _buildStep(width, '3. 만점을 향해 도전해보세요!'),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildStep(double width, String title) {
+    return Container(
+      padding: EdgeInsets.fromLTRB(
+        width * 0.048,
+        width * 0.024,
+        width * 0.048,
+        width * 0.024,
+      ),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Icon(
+            Icons.check_box,
+            size: width * 0.04,
+          ),
+          Padding(
+            padding: EdgeInsets.only(right: width * 0.024),
+          ),
+          Text(title),
+        ],
+      ),
+    );
+  }
+}
